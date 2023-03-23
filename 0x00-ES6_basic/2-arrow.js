0b2aa89ee31b5b8c25ce34a5f63cff897cc4da31
Tishly <ekaetteessien27@gmail.com>
2023-03-23 06:44:26 +0100

    modified function to anonymous arrow function

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,7 +1,8 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  const self = this;
-  this.addNeighborhood = (newNeighborhood) => self.sanFranciscoNeighborhoods.push(newNeighborhood);
-  // self.sanFranciscoNeighborhoods;
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
 }
