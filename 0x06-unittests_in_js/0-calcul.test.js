af730befa61ea261a9a39af020c13e6a796024f9
Tishly <ekaetteessien27@gmail.com>
2023-05-20 04:50:53 +0100

    fixed mocha configuratioon

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,35 +1,35 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe("Tests calculateNumber function:", function() {
-    describe("Calculates two integers:", function() {
-        it('Returns 4.', function() {
-            assert.strictEqual(calculateNumber(1, 3), 4);
-        });
+describe('Tests calculation:', () => {
+  describe('Calculates two integers:', () => {
+    it('Returns 4.', () => {
+      assert.strictEqual(calculateNumber(1, 3), 4);
     });
-    describe("Calculates one float & one integer:", function() {
-        it('Returns 5.', function() {
-            assert.strictEqual(calculateNumber(1, 3.7), 5);
-        });
+  });
+  describe('Calculates one float & one integer:', () => {
+    it('Returns 5.', () => {
+      assert.strictEqual(calculateNumber(1, 3.7), 5);
     });
-    describe("Calculates one float & one integer (round down):", function() {
-        it('Returns 4.', function() {
-            assert.strictEqual(calculateNumber(1, 3.3), 4);
-        });
+  });
+  describe('Calculates one float & one integer (round down):', () => {
+    it('Returns 4.', () => {
+      assert.strictEqual(calculateNumber(1, 3.3), 4);
     });
-    describe("Calculates one float & one int reversed:", function () {
-    it('Returns 5.', function () {
+  });
+  describe('Calculates one float & one int reversed:', () => {
+    it('Returns 5.', () => {
       assert.strictEqual(calculateNumber(3.7, 1), 5);
     });
   });
-    describe("Calculates two floats:", function() {
-        it('Returns 5.', function() {
-            assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-        });
+  describe('Calculates two floats:', () => {
+    it('Returns 5.', () => {
+      assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     });
-    describe("Calculates two floats w/borderline:", function() {
-        it('Returns 6.', function() {
-            assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-        });
+  });
+  describe('Calculates two floats w/borderline:', () => {
+    it('Returns 6.', () => {
+      assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
+  });
 });
